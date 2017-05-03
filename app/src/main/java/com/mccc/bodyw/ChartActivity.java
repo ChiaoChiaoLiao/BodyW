b46bd0ed941b740697e6ed2cb2a69d66bcc90dde
ChuChiao Liao <ChuChiao_Liao@asus.com>
2017-05-03 18:40:39 +0800

    Patch empty data if skip days

--- app/src/main/java/com/mccc/bodyw/ChartActivity.java
+++ app/src/main/java/com/mccc/bodyw/ChartActivity.java
@@ -47,11 +47,13 @@ public class ChartActivity extends AppCompatActivity
         }
     };
     private class RecordData {
+        private int index;
         private int date;
         private int weight;
         private int bodyFat;
 
-        private RecordData(int date, int weight, int bodyFat) {
+        private RecordData(int index, int date, int weight, int bodyFat) {
+            this.index = index;
             this.date = date;
             this.weight = weight;
             this.bodyFat = bodyFat;
@@ -59,7 +61,6 @@ public class ChartActivity extends AppCompatActivity
     }
     private LineChart mLineChart;
     private List<RecordData> mRecordDataList;
-    private FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +69,8 @@ public class ChartActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mFab = (FloatingActionButton) findViewById(R.id.fab);
-        mFab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), InputActivity.class));
@@ -93,10 +94,8 @@ public class ChartActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
-
         getContentResolver().registerContentObserver(
                 AttributeContentProvider.RECORD_URI, true, mContentObserver);
-
     }
 
     @Override
@@ -191,10 +190,9 @@ public class ChartActivity extends AppCompatActivity
     }
 
     private void refreshData(Cursor cursor) {
-        mRecordDataList = getCursorData(cursor);
+        List<Integer> dataIndexList = getCursorData(cursor);
         mLineChart.setNoDataText("");
         if (mRecordDataList.size() == 0) {
-            mFab.callOnClick();
             return;
         }
         ILineDataSet bodyWeightData = setBodyWeightData();
@@ -202,38 +200,129 @@ public class ChartActivity extends AppCompatActivity
         LineData recordData = new LineData();
         recordData.addDataSet(bodyWeightData);
         recordData.addDataSet(bodyFatData);
-        drawRecordChart(mLineChart, cursor.getCount());
+        drawRecordChart(mLineChart, dataIndexList,
+                mRecordDataList.get(mRecordDataList.size()-1).index + 1);
         mLineChart.setData(recordData);
         mLineChart.invalidate();
     }
 
-    private List<RecordData> getCursorData(Cursor cursor) {
-        List<RecordData> recordDataList = new ArrayList<>();
-        while (cursor != null && cursor.moveToNext()) {
-            recordDataList.add(new RecordData(cursor.getInt(MainDatabaseHelper.RecordQuery.DATE),
-                    cursor.getInt(MainDatabaseHelper.RecordQuery.WEIGHT),
-                    cursor.getInt(MainDatabaseHelper.RecordQuery.BODY_FAT)));
+    private List<Integer> getCursorData(Cursor cursor) {
+        List<Integer> indexDataList = new ArrayList<>();
+        int index = 0;
+        int count = 0;
+        if (cursor != null) {
+            cursor.moveToFirst();
         }
-        return recordDataList;
+        while (cursor != null && cursor.moveToNext()) {
+            int tempIndex;
+            if (count > 0) {
+                tempIndex = calIndexByDate(index, count,
+                        cursor.getInt(MainDatabaseHelper.RecordQuery.DATE));
+                mRecordDataList.add(new RecordData(
+                        tempIndex,
+                        cursor.getInt(MainDatabaseHelper.RecordQuery.DATE),
+                        cursor.getInt(MainDatabaseHelper.RecordQuery.WEIGHT),
+                        cursor.getInt(MainDatabaseHelper.RecordQuery.BODY_FAT)));
+                index = tempIndex;
+            } else {
+                mRecordDataList.add(new RecordData(
+                        0,
+                        cursor.getInt(MainDatabaseHelper.RecordQuery.DATE),
+                        cursor.getInt(MainDatabaseHelper.RecordQuery.WEIGHT),
+                        cursor.getInt(MainDatabaseHelper.RecordQuery.BODY_FAT)));
+            }
+            count = count + 1;
+            indexDataList.add(index);
+        }
+        return indexDataList;
     }
+
+    private int calIndexByDate(int lastIndex, int count, int nowDate) {
+        int lastDate = mRecordDataList.get(count-1).date;
+        int lastYear = lastDate / 10000;
+        int lastMonth = (lastDate % 10000) / 100;
+        int lastDay = lastDate % 100;
+
+        int nowYear = nowDate / 10000;
+        int nowMonth = (nowDate % 10000) / 100;
+        int nowDay = nowDate % 100;
+
+        if (lastYear == nowYear && lastMonth == nowMonth) {
+            return lastIndex + (nowDay - lastDay);
+        } else if (lastYear == nowYear) {
+            for (int i = 1; i < nowMonth - lastMonth + 1; i++) {
+                int tempMonth = nowMonth - i;
+                nowDay = nowDay + daysOfMonth(lastYear, tempMonth);
+            }
+            return lastIndex + (nowDay - lastDay);
+        } else {
+            int gapDays = 0;
+            for (int i = 1; i < nowYear - lastYear + 1; i++) {
+                int tempYear = nowYear - i;
+                if (tempYear % 400 == 0 || (tempYear % 4 == 0 && tempYear % 100 != 0)) {
+                    gapDays = gapDays + 366;
+                } else {
+                    gapDays = gapDays + 365;
+                }
+                if (i == nowYear - lastYear) {
+                    for (int j = 1; j < lastMonth + 1; j++) {
+                        gapDays = gapDays - daysOfMonth(lastYear, j);
+                    }
+                    gapDays = gapDays + lastDay;
+                    for (int j = 1; j < nowMonth; j++) {
+                        gapDays = gapDays + daysOfMonth(nowYear, j);
+                    }
+                    gapDays = gapDays + nowDay;
+                }
+            }
+            return lastIndex + gapDays;
+        }
+    }
+
+    private int daysOfMonth(int year, int month) {
+        if (month == 1 || month == 3 || month == 5 || month == 7
+                || month == 8 || month == 10 || month == 12) {
+            return 31;
+        } else if (month == 2) {
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                return 29;
+            } else {
+                return 28;
+            }
+        } else {
+            return  30;
+        }
+    }
+
+    private static final float DATA_SET_LINE_WIDTH = 3;
+    private static final float DATA_SET_CIRCLE_RADIUS = 5;
+    private static final int DATA_SET_LEFT_AXIS_COLOR = Color.CYAN;
+    private static final int DATA_SET_RIGHT_AXIS_COLOR = Color.YELLOW;
+    private static final float CHART_VIEW_POINT_NUM = 7;
+    private static final float CHART_BORDER_WIDTH = 2;
+    private static final float CHART_AXIS_LABEL_WIDTH = 40;
+    private static final int CHART_BORDER_COLOR = Color.LTGRAY;
+    private static final float CHART_ROTATION_ANGLE = -40;
 
     private ILineDataSet setBodyWeightData() {
         List<Entry> entries = new ArrayList<>();
 
         for (int i = 0; i < mRecordDataList.size(); i++) {
-            entries.add(new Entry(i, mRecordDataList.get(i).weight));
+            if (mRecordDataList.get(i).weight == 0) {
+                continue;
+            }
+            entries.add(new Entry(mRecordDataList.get(i).index, mRecordDataList.get(i).weight));
         }
 
         LineDataSet dataSet = new LineDataSet(entries, "Body Weight");
-        dataSet.setColor(Color.CYAN);
-        dataSet.setLineWidth(3f);
-        dataSet.setCircleColor(Color.CYAN);
-        dataSet.setCircleRadius(5f);
-        dataSet.setCircleColorHole(Color.CYAN);
+        dataSet.setColor(DATA_SET_LEFT_AXIS_COLOR);
+        dataSet.setLineWidth(DATA_SET_LINE_WIDTH);
+        dataSet.setCircleColor(DATA_SET_LEFT_AXIS_COLOR);
+        dataSet.setCircleRadius(DATA_SET_CIRCLE_RADIUS);
+        dataSet.setCircleColorHole(DATA_SET_LEFT_AXIS_COLOR);
         dataSet.setMode(LineDataSet.Mode.LINEAR);
         dataSet.setDrawValues(false);
-        dataSet.setDrawVerticalHighlightIndicator(false);
-        dataSet.setHighLightColor(Color.CYAN);
+        dataSet.setHighLightColor(DATA_SET_LEFT_AXIS_COLOR);
 
         dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
         TextView leftAxis = (TextView) findViewById(R.id.left_axis);
@@ -246,19 +335,21 @@ public class ChartActivity extends AppCompatActivity
         List<Entry> entries = new ArrayList<>();
 
         for (int i = 0; i < mRecordDataList.size(); i++) {
-            entries.add(new Entry(i, mRecordDataList.get(i).bodyFat));
+            if (mRecordDataList.get(i).bodyFat == 0) {
+                continue;
+            }
+            entries.add(new Entry(mRecordDataList.get(i).index, mRecordDataList.get(i).bodyFat));
         }
 
         LineDataSet dataSet = new LineDataSet(entries, "Body Fat");
-        dataSet.setColor(Color.YELLOW);
-        dataSet.setLineWidth(3f);
-        dataSet.setCircleColor(Color.YELLOW);
-        dataSet.setCircleRadius(5f);
-        dataSet.setCircleColorHole(Color.YELLOW);
+        dataSet.setColor(DATA_SET_RIGHT_AXIS_COLOR);
+        dataSet.setLineWidth(DATA_SET_LINE_WIDTH);
+        dataSet.setCircleColor(DATA_SET_RIGHT_AXIS_COLOR);
+        dataSet.setCircleRadius(DATA_SET_CIRCLE_RADIUS);
+        dataSet.setCircleColorHole(DATA_SET_RIGHT_AXIS_COLOR);
         dataSet.setMode(LineDataSet.Mode.LINEAR);
         dataSet.setDrawValues(false);
-        dataSet.setDrawVerticalHighlightIndicator(false);
-        dataSet.setHighLightColor(Color.YELLOW);
+        dataSet.setHighLightColor(DATA_SET_RIGHT_AXIS_COLOR);
 
         dataSet.setAxisDependency(YAxis.AxisDependency.RIGHT);
         TextView rightAxis = (TextView) findViewById(R.id.right_axis);
@@ -267,16 +358,16 @@ public class ChartActivity extends AppCompatActivity
         return dataSet;
     }
 
-    private void drawRecordChart(LineChart chart, int size) {
+    private void drawRecordChart(LineChart chart, final List<Integer> datIndexList, final int size) {
         chart.getDescription().setEnabled(false);
         chart.setBackgroundColor(Color.TRANSPARENT);
         chart.setDrawGridBackground(false);
         chart.setDragEnabled(true);
         chart.setScaleEnabled(false);
         chart.setDrawBorders(true);
-        chart.setBorderColor(Color.LTGRAY);
-        chart.setBorderWidth(2);
-        chart.setScaleMinima(size/7.25f, 1);
+        chart.setBorderColor(CHART_BORDER_COLOR);
+        chart.setBorderWidth(CHART_BORDER_WIDTH);
+        chart.setScaleMinima(size / (CHART_VIEW_POINT_NUM + 0.25f), 1);
 
         Legend legend = chart.getLegend();
         legend.setWordWrapEnabled(true);
@@ -284,40 +375,42 @@ public class ChartActivity extends AppCompatActivity
         legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
         legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
         legend.setDrawInside(false);
-        legend.setTextColor(Color.LTGRAY);
+        legend.setTextColor(CHART_BORDER_COLOR);
         legend.setForm(Legend.LegendForm.LINE);
         legend.setXEntrySpace(30);
 
         YAxis rightAxis = chart.getAxisRight();
         rightAxis.setDrawGridLines(false);
-        rightAxis.setMinWidth(30);
-        rightAxis.setMaxWidth(30);
-        rightAxis.setTextColor(Color.LTGRAY);
+        rightAxis.setMinWidth(CHART_AXIS_LABEL_WIDTH);
+        rightAxis.setMaxWidth(CHART_AXIS_LABEL_WIDTH);
+        rightAxis.setTextColor(CHART_BORDER_COLOR);
         rightAxis.setAxisLineWidth(1);
-        rightAxis.setAxisLineColor(Color.YELLOW);
+        rightAxis.setAxisLineColor(DATA_SET_RIGHT_AXIS_COLOR);
 
         YAxis leftAxis = chart.getAxisLeft();
         leftAxis.setDrawGridLines(false);
-        leftAxis.setMinWidth(30);
-        leftAxis.setMaxWidth(30);
-        leftAxis.setTextColor(Color.LTGRAY);
+        leftAxis.setMinWidth(CHART_AXIS_LABEL_WIDTH);
+        leftAxis.setMaxWidth(CHART_AXIS_LABEL_WIDTH);
+        leftAxis.setTextColor(CHART_BORDER_COLOR);
         leftAxis.setAxisLineWidth(1);
-        leftAxis.setAxisLineColor(Color.CYAN);
+        leftAxis.setAxisLineColor(DATA_SET_LEFT_AXIS_COLOR);
 
         XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setDrawGridLines(false);
-        xAxis.setAxisMinimum(mRecordDataList.size()-7 >= 0? 0 : mRecordDataList.size()-7);
-        xAxis.setAxisMaximum(mRecordDataList.size()-1);
-        xAxis.setTextColor(Color.LTGRAY);
-        xAxis.setLabelRotationAngle(-40);
+        xAxis.setAxisMinimum(size - CHART_VIEW_POINT_NUM >= 0? -1 : size - CHART_VIEW_POINT_NUM - 1);
+        xAxis.setAxisMaximum(size);
+        xAxis.setTextColor(CHART_BORDER_COLOR);
+        xAxis.setLabelRotationAngle(CHART_ROTATION_ANGLE);
         xAxis.setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
-                if ((int) value < 0 || (int) value > mRecordDataList.size()) {
+                int index = (int) value;
+                if (index < 0 || index > size - 1 || !datIndexList.contains(index)) {
                     return "";
                 }
-                return String.valueOf(mRecordDataList.get((int) value).date).substring(4);
+                return String.valueOf(mRecordDataList.get(datIndexList.indexOf(index)).date)
+                        .substring(4);
             }
         });
 
